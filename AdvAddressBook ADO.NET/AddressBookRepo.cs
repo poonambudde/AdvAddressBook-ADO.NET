803405943281171a5be110defdf5e034e66cd289
poonambudde <buddepoonam123@gmail.com>
2022-03-30 17:39:59 +0530

    [Poonam] Added: Insert new contact in AddressBook

--- AdvAddressBook ADO.NET/AddressBookRepo.cs
+++ AdvAddressBook ADO.NET/AddressBookRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
@@ -7,8 +8,12 @@ namespace AdvAddressBook_ADO.NET
 {
     public class AddressBookRepo
     {
+        //Give path for Database Connection
         public static string connectionString = @"Data Source=(Localdb)\MSSQLLocalDB;Initial Catalog=AddressBook_Service;Integrated Security=True";
+        //Represents a connection to Sql Server Database
         SqlConnection connection = new SqlConnection(connectionString);
+
+        // Checks the connection.
         public void DataBaseConnection()
         {
             try
@@ -20,6 +25,47 @@ namespace AdvAddressBook_ADO.NET
             catch (Exception e)
             {
                 Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        // Adds the new contact to data base.
+        public bool addNewContactToDataBase(AddressBookModel addressBookModel)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand cmd = new SqlCommand("SpAddAddressBookDetails", this.connection);
+                    //setting command type as stored procedure
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@FirstName", addressBookModel.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", addressBookModel.LastName);
+                    cmd.Parameters.AddWithValue("@Address", addressBookModel.Address);
+                    cmd.Parameters.AddWithValue("@City", addressBookModel.City);
+                    cmd.Parameters.AddWithValue("@State", addressBookModel.State);
+                    cmd.Parameters.AddWithValue("@Zip", addressBookModel.Zip);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", addressBookModel.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@Email", addressBookModel.Email);
+                    cmd.Parameters.AddWithValue("@AddressBookName", addressBookModel.AddressBookName);
+                    cmd.Parameters.AddWithValue("@AddressBookType", addressBookModel.AddressBookType);
+                    this.connection.Open();
+                    //Return the number of rows updated
+                    var result = cmd.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
             }
         }
     }
--- AdvAddressBook ADO.NET/Program.cs
+++ AdvAddressBook ADO.NET/Program.cs
@@ -9,7 +9,19 @@ namespace AdvAddressBook_ADO.NET
             Console.WriteLine("Welcome TO AddressBook Problems Using ADO.NET");
 
             AddressBookRepo addressBookRepo = new AddressBookRepo();
+            AddressBookModel addressBookModel = new AddressBookModel();
+            addressBookModel.FirstName = "Akash";
+            addressBookModel.LastName = "Sangale";
+            addressBookModel.Address = "Tawarja";
+            addressBookModel.City = "latur";
+            addressBookModel.State = "Maharashtra";
+            addressBookModel.Zip = 413512;
+            addressBookModel.PhoneNumber = 8149713160;
+            addressBookModel.Email = "dhiraj@gmail.com";
+            addressBookModel.AddressBookName = "friend address book";
+            addressBookModel.AddressBookType = "Friend";
             addressBookRepo.DataBaseConnection();
+            addressBookRepo.addNewContactToDataBase(addressBookModel);
         }
     }
 }
